ccd30c811b90870c3f30ed1de0169daa80e8b0bf
A J Henderson <jahenderson777@gmail.com>
2025-02-21 18:17:40 +0000

    .

--- test/ascii_radar/core_test.clj
+++ test/ascii_radar/core_test.clj
@@ -33,8 +33,31 @@ o-o--o-o")
                        (apply str (str/split-lines (str/trim invader-1))))))
     (is (= 1.0
            (core/score (apply str (str/split-lines (str/trim invader-2)))
-                       (apply str (str/split-lines (str/trim invader-2))))))) )
-
+                       (apply str (str/split-lines (str/trim invader-2)))))))
+  
+  (testing "changing just one location at the end reduces the score appropriately"
+    (is (= (float 0.9886364)
+           (core/score (apply str (str/split-lines (str/trim invader-1)))
+            (apply str (str/split-lines (str/trim "--o-----o--
+---o---o---
+--ooooooo--
+-oo-ooo-oo-
+ooooooooooo
+o-ooooooo-o
+o-o-----o-o
+---oo-oo--o")))))))
+  
+  (testing "changing just one location at the beginning reduces the score appropriately"
+    (is (= (float 0.9886364)
+           (core/score (apply str (str/split-lines (str/trim invader-1)))
+            (apply str (str/split-lines (str/trim "--o-----o--
+o--o---o---
+--ooooooo--
+-oo-ooo-oo-
+ooooooooooo
+o-ooooooo-o
+o-o-----o-o
+---oo-oo---"))))))))
 
 
 (run-tests)
