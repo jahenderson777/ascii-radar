dc5a1ddd77219fc312ef6b4aa41da85408b427dc
A J Henderson <jahenderson777@gmail.com>
2025-02-22 22:09:09 +0000

    .

--- src/ascii_radar/core.clj
+++ src/ascii_radar/core.clj
@@ -1,125 +1,27 @@
 (ns ascii-radar.core
-  (:require [clojure.string :as str])
-  
+  (:require [ascii-radar.sample-data :as sample-data]
+            [clojure.string :as str]) 
   (:gen-class))
 
 (def detection-threshold 0.75)
 
-(def known-invaders
-  ["
---o-----o--
----o---o---
---ooooooo--
--oo-ooo-oo-
-ooooooooooo
-o-ooooooo-o
-o-o-----o-o
----oo-oo---"
-   
-   "
----oo---
---oooo--
--oooooo-
-oo-oo-oo
-oooooooo
---o--o--
--o-oo-o-
-o-o--o-o"
-   ])
-
-(def radar 
-  "----o--oo----o--ooo--ooo--o------o---oo-o----oo---o--o---------o----o------o-------------o--o--o--o-
---o-o-----oooooooo-oooooo---o---o----o------ooo-o---o--o----o------o--o---ooo-----o--oo-o------o----
---o--------oo-ooo-oo-oo-oo-----O------------ooooo-----oo----o------o---o--o--o-o-o------o----o-o-o--
--------o--oooooo--o-oo-o--o-o-----oo--o-o-oo--o-oo-oo-o--------o-----o------o-ooooo---o--o--o-------
-------o---o-ooo-ooo----o-----oo-------o---oo-ooooo-o------o----o--------o-oo--ooo-oo-------------o-o
--o--o-----o-o---o-ooooo-o-------oo---o---------o-----o-oo-----------oo----ooooooo-ooo-oo------------
-o-------------ooooo-o--o--o--o-------o--o-oo-oo-o-o-o----oo------------o--oooo--ooo-o----o-----o--o-
---o-------------------------oo---------oo-o-o--ooo----oo----o--o--o----o--o-o-----o-o------o-o------
--------------------o----------o------o--o------o--------o--------o--oo-o-----oo-oo---o--o---o-----oo
-----------o----------o---o--------------o--o----o--o-o------------oo------o--o-o---o-----o----------
-------o----o-o---o-----o-o---o-----oo-o--------o---------------------------------o-o-o--o-----------
----------------o-------o-----o-------o-------------------o-----o---------o-o-------------o-------oo-
--o--o-------------o-o-----o--o--o--oo-------------o----ooo----o-------------o----------oo----o---o-o
--o--o-------------o----oo------o--o-------o--o-----o-----o----o-----o--o----o--oo-----------o-------
--o-----oo-------o------o----o----------o--o----o-----o-----o-------o-----------o---o-o--oooooo-----o
--o--------o-----o-----o---------oo----oo---o-o---------o---o--oooo-oo--o-------o------oo--oo--o-----
-------------o---------o---------o----oooo-------------oo-oo-----ooo-oo-----o-------o-oo-oooooooo---o
-----------------------o------------oooooooo---o-----o-------o--oooooo-o------------o-o-ooooooo-o----
-------------o------o---o---o-------oo-oo--o--o---------o--o-o-o-ooooo-o--------------oo-o----o-oo-o-
----o-o----------oo-------oo----o----oooooooo-------o----o-o-o-o-----o-o-----o----------ooo-oo--o---o
--o-o---------o-o---------------o--o--o--ooo---ooo-------o------oo-oo------------o--------o--o-o--o--
--------oo---------------------------o-oo----------o------o-o-------o-----o----o-----o-oo-o-----o---o
----o--------o-----o-------o-oo-----oo--oo-o----oo----------o--o---oo------oo----o-----o-------o-----
----o--ooo-o---------o-o----o------------o---------o----o--o-------o----o--------o----------------oo-
----o------o----------------o----o------o------o---oo-----------o-------------o----------oo---------o
---oo---------------o--o------o---o-----o--o-------------o------o-------o-----o-----o----o------o--o-
--o-------o----------o-o-o-------o-----o--o-o-----------o-oo-----------o------o---------o-----o-o----
-----------o----o-------o----o--o------o------------o---o---------------oo----o-----ooo--------------
-----o--------oo----o-o----o--o------ooo----o-oooo---o--o-oo--------o-oo-----o-o---o-o--o-----oo-----
-------o--------o-ooooo----o---o--o-----o---------------o-o-------o-----o----------------------------
-o-------oo----o--oooooo-o---o--o------oooo----------o-oo-------o---o----------o------oo-------------
--o---o----------o--oo-oo-o---o-----o-o-----------------------oo--o------o------o--------------------
------oo-o-o-o---ooooooooo----o----o--------o--o---oo---o------------o----------o-o---o------o-o--oo-
-------o------o---ooo-o---------------------------o--o---o---o----o--o-------o-----o------o----o----o
--------o----------ooo-o-----o----o---o--o-oo--o--o-o--o------o--o-oo---ooo------------------------o-
--o-------o------o-o--ooo--o---o---oo-----o----o-------------o----o-ooo-o------o--o-o------o-o-------
----oo--o---o-o---------o---o--------------o--o-----o-------o-----o--o---o-oo--------o----o----o-----
-o------o----oo-o-----------oo--o---o--------o-o------o-------o-o------o-oo---------o-----oo---------
-----o--o---o-o-----------o---o------------o-------o----o--o--o--o-o---------------o-----------------
--------oo--o-o-----o-----o----o-o--o----------------------o-------o------o----oo----ooo---------o---
-o-----oo-------------------o--o-----o-----------o------o-------o----o-----------o----------------o--
---o---o-------o------------o--------------------o----o--o-------------oo---o---------oo--------o----
---o--------o---------o------------o------o-------o------------o-------o---o---------ooooo-----------
-------o--------------o-o-o---------o---o-------o--o-----o-------o-o----------o-----oo-ooo----------o
---o---------------o----o--oo-------------o---------o-------------------oo---------oo-o-ooo----------
--o-----------o------ooo----o----------------ooo-----o--------o--o---o-----------o-o-oooooo--------oo
--o---o-------o---o-oooo-----o-------------------o----oo-----------------o--o--------o--o------o--o--
--------o---o------oooooo--o----ooo--o--------o-------o----------------------------oo-oo-o--o--------
-o--oo------o-----oo--o-oo------------oo--o------o--o-------------oo----o------------oooo-o------oo--
------o----------ooooooooo--------------oo--------------oo-----o-----o-o--o------o----------o----o---")
-
-
 (defn score [a b]
   (let [paired (map vector a b)
         s (reduce (fn [score [ax bx]]
-                    (if (or (= ax \U)
-                            (= bx \U))
-                      score ;; we have an Unknown location, e.g. beyond the edge
-                      (if (= ax bx)
-                        (+ score 1) ;; we have a match
-                        score ;; we have no match
-                        )))
+                    (if (= ax bx) ; we have a match
+                      (+ score 1) 
+                      score))
                   0 paired)]
     (float (/ s (count a)))))
 
-(comment
-  (score (apply str (str/split-lines (str/trim (nth known-invaders 0))))
-         (apply str (str/split-lines (str/trim "
---------oo-
----o---o---
---ooooooo--
--oo-ooo-oo-
-ooooooooooo
-o-ooooooo-o
-o-o-----o-o
-o--oo-oooo-")))
-         )
-  )
-
-(defn add-unknowns [sample w h]
-  (let [lines (str/split-lines sample)
-        middle (map (fn [l]
-                      (let [u (apply str (repeat w "U"))]
+(defn add-unknowns [radar w h]
+  (let [middle (map (fn [l]
+                      (let [u (str/join (repeat w "U"))]
                         (str u l u)))
-                    lines)
-        start-end (repeat h (apply str (repeat (+ (count (first lines)) (* 2 w)) 
-                                               "U")))]
+                    radar)
+        final-w (+ (count (first radar)) (* 2 w))
+        start-end (repeat h (str/join (repeat final-w "U")))]
     (vec (concat start-end middle start-end))))
-
-(comment
-  (add-unknowns radar 6 10)
-  )
 
 (defn sub-sample [sample w h x y] 
   (let [lines (subvec sample y (+ y h))]
@@ -133,24 +35,21 @@ o--oo-oooo-")))
       [x y])))
 
 (defn scan-for-invader [radar invader threshold]
-  (let [invader (str/trim invader)
-        invader-lines (str/split-lines invader)
-        invader-w (count (first invader-lines))
-        invader-h (count invader-lines)
-        locations (all-locations (str/split-lines radar) invader-w invader-h)
-        radar (add-unknowns radar invader-w invader-h)]
-    (reduce (fn [v [x y]]
-              (let [samp (sub-sample radar invader-w invader-h x y)
-                    s (score (apply str samp) (apply str invader-lines))]
-                (if (>= s threshold)
-                  (conj v {:location [(- x invader-w) 
-                                      (- y invader-h)] 
-                           :score s
-                           :invader invader
-                           :sample (str/join "\n" samp)})
-                  v)))
-            []
-            locations)))
+  (let [invader-w (count (first invader))
+        invader-h (count invader)
+        radar' (add-unknowns radar invader-w invader-h)]
+    (->> (all-locations radar invader-w invader-h)
+         (reduce (fn [v [x y]]
+                   (let [samp (sub-sample radar' invader-w invader-h x y)
+                         s (score (str/join samp) (str/join invader))]
+                     (if (>= s threshold)
+                       (conj v {:location [(- x invader-w) 
+                                           (- y invader-h)] 
+                                :score s
+                                :invader (str/join "\n" invader)
+                                :sample (str/join "\n" samp)})
+                       v)))
+                 []))))
 
 (defn print-results [results]
   (doseq [{:keys [location score invader sample]} results]
@@ -159,21 +58,30 @@ o--oo-oooo-")))
     (println "")
     (println sample)))
 
-
 (doall (map (fn [invader]
-              (print-results (scan-for-invader radar invader 0.6)))
-            known-invaders))
+              (print-results (scan-for-invader sample-data/radar invader detection-threshold)))
+            [sample-data/invader-1 sample-data/invader-2]))
 
-(doall
- (scan-for-invader (str/trim "-o\n--")
-                   (str/trim "-o\n--") 
-                   0.0)
- 
+(scan-for-invader ["o"] ["o"] 0.0)
 
- )
 
-(scan-for-invader "o" "o" 0.0)
 
-(defn -main [& args]
-  )
+(defn gen-radar [w h]
+  (repeat h (str/join (repeat w "-"))))
 
+(gen-radar 10 10)
+
+(defn add-invader-to-radar [radar invader x y]
+  (map-indexed (fn [idx l]
+                 (let [invader-w (count (first invader))
+                       invader-h (count invader)
+                       y' (- idx y)]
+                   (if (and (>= y' 0) (< y' invader-h))
+                     (let [invader-l (nth invader y')]
+                       (str (subs l 0 x) invader-l (subs l (+ x invader-w))))
+                     l)))
+               radar))
+
+(add-invader-to-radar (gen-radar 10 10) ["oo" "oo"] 4 4)
+
+(defn -main [& args])
--- test/ascii_radar/core_test.clj
+++ test/ascii_radar/core_test.clj
@@ -1,61 +1,39 @@
 (ns ascii-radar.core-test
   (:require [clojure.test :refer [deftest is run-tests testing]]
             [ascii-radar.core :as core]
+            [ascii-radar.sample-data :as sample-data]
             [clojure.string :as str]))
-
-(def invader-1
-  "--o-----o--
----o---o---
---ooooooo--
--oo-ooo-oo-
-ooooooooooo
-o-ooooooo-o
-o-o-----o-o
----oo-oo---")
-
-(def invader-2
-  "
----oo---
---oooo--
--oooooo-
-oo-oo-oo
-oooooooo
---o--o--
--o-oo-o-
-o-o--o-o")
 
 (deftest test-score 
   (testing "testing that score gives 1.0 when both inputs are equal"
     (is (= 1.0
-           (core/score (apply str (str/split-lines (str/trim invader-1)))
-                       (apply str (str/split-lines (str/trim invader-1))))))
+           (core/score (str/join sample-data/invader-1) (str/join sample-data/invader-1))))
     (is (= 1.0
-           (core/score (apply str (str/split-lines (str/trim invader-2)))
-                       (apply str (str/split-lines (str/trim invader-2)))))))
+           (core/score sample-data/invader-2 sample-data/invader-2))))
   
   (testing "changing just one location at the end reduces the score appropriately"
     (is (= (float 0.9886364)
-           (core/score (apply str (str/split-lines (str/trim invader-1)))
-            (apply str (str/split-lines (str/trim "--o-----o--
----o---o---
---ooooooo--
--oo-ooo-oo-
-ooooooooooo
-o-ooooooo-o
-o-o-----o-o
----oo-oo--o")))))))
+           (core/score (str/join sample-data/invader-1)
+            (str/join ["--o-----o--"
+                       "---o---o---"
+                       "--ooooooo--"
+                       "-oo-ooo-oo-"
+                       "ooooooooooo"
+                       "o-ooooooo-o"
+                       "o-o-----o-o"
+                       "---oo-oo--o"])))))
   
   (testing "changing just one location at the beginning reduces the score appropriately"
     (is (= (float 0.9886364)
-           (core/score (apply str (str/split-lines (str/trim invader-1)))
-            (apply str (str/split-lines (str/trim "--o-----o--
-o--o---o---
---ooooooo--
--oo-ooo-oo-
-ooooooooooo
-o-ooooooo-o
-o-o-----o-o
----oo-oo---"))))))))
+           (core/score (str/join sample-data/invader-1)
+            (str/join ["o-o-----o--"
+                       "---o---o---"
+                       "--ooooooo--"
+                       "-oo-ooo-oo-"
+                       "ooooooooooo"
+                       "o-ooooooo-o"
+                       "o-o-----o-o"
+                       "---oo-oo---"]))))))
 
 
 (deftest test-sub-sample
@@ -70,7 +48,7 @@ o---------
 --o-------
 ----------
 ----------"
-           (str/join "\n" (core/sub-sample (str/split-lines core/radar) 10 10 0 0)))))
+           (str/join "\n" (core/sub-sample sample-data/radar 10 10 0 0)))))
   
   (testing "basic sub-sample, should be not equal"
     (is (not= "----o--oo-
@@ -83,8 +61,7 @@ o---------
   --o-------
   ----------
   ----------"
-           (str/join "\n" (core/sub-sample (str/split-lines core/radar) 11 10 10 10))))))
-
+           (str/join "\n" (core/sub-sample sample-data/radar 11 10 10 10))))))
 
 
 (deftest test-scan-for-invader
@@ -94,7 +71,7 @@ o---------
             {:location [-1 0], :score 0.0, :invader "o", :sample "U"}
             {:location [0 0], :score 1.0, :invader "o", :sample "o"}]
            
-           (core/scan-for-invader "o" "o" 0.0))))
+           (core/scan-for-invader ["o"] ["o"] 0.0))))
   
   (testing "invader bigger than radar"
     (is (= [{:location [-1 -2], :score 0.0, :invader "o\no", :sample "U\nU"}
@@ -104,6 +81,16 @@ o---------
             {:location [-1 0], :score 0.0, :invader "o\no", :sample "U\nU"}
             {:location [0 0], :score 0.5, :invader "o\no", :sample "o\nU"}]
   
-           (core/scan-for-invader "o" "o\no" 0.0)))))
+           (core/scan-for-invader ["o"] ["o" "o"] 0.0))))
+  
+  (testing "invader wider than radar"
+    (is (= [{:location [-2 -1], :score 0.0, :invader "oo", :sample "UU"}
+            {:location [-1 -1], :score 0.0, :invader "oo", :sample "UU"}
+            {:location [0 -1], :score 0.0, :invader "oo", :sample "UU"}
+            {:location [-2 0], :score 0.0, :invader "oo", :sample "UU"}
+            {:location [-1 0], :score 0.5, :invader "oo", :sample "Uo"}
+            {:location [0 0], :score 0.5, :invader "oo", :sample "oU"}]
+
+           (core/scan-for-invader ["o"] ["oo"] 0.0)))))
 
 (run-tests)
